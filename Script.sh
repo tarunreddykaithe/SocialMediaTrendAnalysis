b22344a9740092c1bbdb1c1f8575e2b57f6f17e0
Tarun Reddy <tarunreddykaithe007@gmail.com>
2020-04-22 01:05:45 -0700

    clean repository

--- Script.sh
+++ Script.sh
@@ -1,7 +1,7 @@
-python PopularHashtagsStreamer.py
+python GeoBasedStreamer.py
 sleep 90
 PYSPARK_PYTHON=python3.6 spark-submit
  --master yarn
  --deploy-mode client --conf "spark.dynamicAllocation.enabled=false"
  --jars spark-streaming-kafka-0-8-assembly_2.11-2.3.1.jar
-  PopularHashtagsProcessor.py sandbox-hdp.hortonworks.com:2181 PopularHashtags
+  GeoBasedProcessor.py sandbox-hdp.hortonworks.com:2181 geoBasedTweets
